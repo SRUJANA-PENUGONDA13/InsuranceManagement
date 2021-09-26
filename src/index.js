8fa7d8efe7ea3f7556832a5bae51a62b988a7eb8
Srujana Penugonda <penugondasrujana3@gmail.com>
2021-09-26 08:58:43 +0530

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 
 // importing user defined router modules
 const { policyRouter } = require('./routers/policy.js')
@@ -7,6 +8,13 @@ const app = express()
 // Database connection
 require('./db/dbConnection')
 
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(policyRouter)
 
